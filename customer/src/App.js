cbea444219b1a7a0d8751198de23c26c060ac652
Alan Hu <on.alanhu@gmail.com>
2019-03-16 15:17:52 +0100

    mend

--- customer/src/App.js
+++ customer/src/App.js
@@ -5,10 +5,24 @@ import Counter from './ui/counter';
 import './App.css';
 
 class App extends Component { // eslint-disable-line
+  state = {
+    count: 0,
+  }
+
+  increment = (n) => {
+    const { count } = this.state;
+    if (count + n >= 0) {
+      this.setState(state => ({
+        count: state.count + n,
+      }));
+    }
+  }
+
   render() {
+    const { count } = this.state;
     return (
       <div className="App">
-        <Counter onClick={() => console.log('hello')} />
+        <Counter count={count} onAdd={() => this.increment(1)} onMinus={() => this.increment(-1)} />
       </div>
     );
   }
--- customer/src/ui/counter.js
+++ customer/src/ui/counter.js
@@ -1,33 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import IconButton from './iconButton';
 
 import '../styles/counter.css';
 
-class Counter extends Component {
-  state = {
-    count: 0,
-  }
-
-  increment = (n) => {
-    const { count } = this.state;
-    if (count + n >= 0) {
-      this.setState(state => ({
-        count: state.count + n,
-      }));
-    }
-  }
-
-  render() {
-    const { count } = this.state;
-    return (
-      <div className="counter">
-        <IconButton iconUrl="/minus.png" onClick={() => this.increment(-1)} />
-        <span className="counter__count">{count}</span>
-        <IconButton iconUrl="/plus.png" onClick={() => this.increment(1)} />
-      </div>
-    );
-  }
-}
+const Counter = ({ count, onMinus, onAdd }) => (
+  <div className="counter">
+    <IconButton iconUrl="/minus.png" onClick={onMinus} />
+    <span className="counter__count">{count}</span>
+    <IconButton iconUrl="/plus.png" onClick={onAdd} />
+  </div>
+);
 
 export default Counter;
