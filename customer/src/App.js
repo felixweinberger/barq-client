f8787c3e65c798ab4bc49447b89a4d15bb009cfe
Alan Hu <on.alanhu@gmail.com>
2019-03-16 13:01:48 +0100

    feat(presentational components): basic primary button done

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import Logo from './ui/logo';
+import PrimaryButton from './ui/primaryButton';
 
 import './App.css';
 
 class App extends Component { // eslint-disable-line
   render() {
     return (
-      <Logo title="BarQ" />
+      <PrimaryButton title="Checkout" onClick={() => console.log('hello')} />
     );
   }
 }
