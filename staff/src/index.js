095d42c2d917619dbed7b67f6ab51dba1e7b6ff2
Natalia <natalia.rizzi88@gmail.com>
2019-03-25 15:26:40 +0100

    login page done, not connected yet to the backend

--- staff/src/index.js
+++ staff/src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducers from './store/reducers';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
 
 ReactDOM.render(
 <Provider store={store}>
