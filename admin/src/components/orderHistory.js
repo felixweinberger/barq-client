66ad53f10ec28175461452f6400fbe2b283e1722
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-26 13:20:30 +0100

    feat(orders): initial version of pretty-print orders

--- admin/src/components/orderHistory.js
+++ admin/src/components/orderHistory.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const OrderHistory = (props) => {
-  const { history } = props;
+  const { history, onRefreshHistory } = props;
   return (
     <div className="orderHistory">
-      {JSON.stringify(history)}
+      <input type="submit" value="Refresh history" onClick={onRefreshHistory} />
+      {history.length > 0
+        ? history.map(order => (
+          <div key={order.orderId}>
+            Order #:
+            {order.orderId}
+            Date:
+            {order.timestamp}
+            Items:
+            {order.items.map(orderItem => (
+              <div key={orderItem._id}>
+                {orderItem.name}
+                x
+                {orderItem.quantity}
+                $
+                {orderItem.price}
+              </div>
+            ))}
+          </div>
+        ))
+        : null}
     </div>
   );
 };
