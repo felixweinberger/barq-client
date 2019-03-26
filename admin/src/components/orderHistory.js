531a24445b9a7bb9992692610bd85c5b86cb1349
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-26 19:22:35 +0100

    style(order history): restyle order hsitory to print nicer history

--- admin/src/components/orderHistory.js
+++ admin/src/components/orderHistory.js
@@ -3,29 +3,37 @@ import React from 'react';
 const OrderHistory = ({ history, refreshHistory }) => (
   <div className="orderHistory">
     <input type="submit" value="Refresh history" onClick={refreshHistory} />
-    <table>
+    <table className="orderHistory__table">
       <tbody>
         <tr>
           <th id="thOrder">#</th>
           <th id="thDate">Date</th>
+          <th id="thTime">Time</th>
           <th id="thItems">Items</th>
           <th id="thTotal">Total</th>
         </tr>
         {history.length > 0
           ? history.map((order, i) => {
-            const total = order.items.reduce((acc, el) => acc + el.amount, 0);
+            const timestamp = new Date(order.timestamp);
+            const date = timestamp.toLocaleDateString('en-GB');
+            const time = timestamp.toLocaleTimeString('en-GB');
+
             return (
               <tr key={order.orderId} className={`tr${i % 2}`}>
                 <td>{order.orderId}</td>
-                <td>{order.timestamp}</td>
-                <td>ORDER ITEMS</td>
-                <td>
-                  €
-                  {total}
+                <td>{date}</td>
+                <td>{time}</td>
+                <td className="orderHistory__itemrow">
+                  {order.items.map(orderItem => (
+                    <div key={orderItem.name}>
+                      {`${orderItem.quantity}x ${orderItem.name} (${orderItem.quantity}x €${orderItem.price})`}
+                    </div>
+                  ))}
                 </td>
+                <td>{`€ ${order.total}`}</td>
               </tr>
             );
-          })
+          }).reverse()
           : null}
       </tbody>
     </table>
