4892470ef4444c4324e5f04cf5fb9d9ecdb23223
Alan Hu <on.alanhu@gmail.com>
2019-04-02 13:04:56 +0200

    style(customer, staff): move images from public to assets

--- customer/src/ui/menuItem/counter.js
+++ customer/src/ui/menuItem/counter.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import IconButton from '../iconButton';
+import minus from '../../assets/minus.png';
+import plus from '../../assets/plus.png';
 
 import '../../styles/counter.css';
 
 const Counter = ({ count, onMinus, onAdd }) => (
   <div className="counter">
-    <IconButton iconUrl="/minus.png" onClick={onMinus} />
+    <IconButton iconUrl={minus} onClick={onMinus} />
     <span className="counter__count">{count}</span>
-    <IconButton iconUrl="/plus.png" onClick={onAdd} />
+    <IconButton iconUrl={plus} onClick={onAdd} />
   </div>
 );
 
