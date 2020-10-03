0f47fb0ee4b86ca6ca5c656b7aa73bb9894bc08f
33702528 <thackrayaf@gmail.com>
2020-10-03 14:13:38 +0100

    Update price.js to correct currency format
    International currency format set as gb-GB so I assumed that this was to be GBP. This could also be updated to an international number format for a country taking the Euro (due to the EUR currency), e.g. es-ES

--- customer/src/ui/price.js
+++ customer/src/ui/price.js
@@ -3,7 +3,7 @@ import React from 'react';
 import '../styles/price.css';
 
 const Price = ({ price, style }) => (
-  <span className="price" style={{ ...style }}>{new Intl.NumberFormat('gb-GB', { style: 'currency', currency: 'EUR' }).format(price)}</span>
+  <span className="price" style={{ ...style }}>{new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(price)}</span>
 );
 
 export default Price;
